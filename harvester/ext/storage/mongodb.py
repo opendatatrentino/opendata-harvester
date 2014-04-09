75218d9343f8a2767b81a8c6bf4dd8be7e3989b1
Samuele Santi <redshadow@hackzine.org>
2014-04-09 15:38:03 +0200

    mongodb storage now supporting collection prefixes

--- harvester/ext/storage/mongodb.py
+++ harvester/ext/storage/mongodb.py
@@ -7,12 +7,24 @@ from .base import BaseStorage
 
 class MongodbStorage(BaseStorage):
     def __init__(self, url):
+        """
+        MongoDB storage takes a URL like:
+
+        mongodb://host:port/database.name/collection.name
+        """
         parsed_url = urlparse.urlparse(url)
         parsed_path = parsed_url.path.strip('/').split('/')
-        if len(parsed_path) != 1:
-            raise ValueError("Invalid MongoDB url (db name)")
+
+        if len(parsed_path) < 1:
+            raise ValueError("Invalid MongoDB url (missing database name)")
+
         self._mongo_url = parsed_url._replace(path='').geturl()
         self._mongo_db_name = parsed_path[0]
+
+        if len(parsed_path) >= 2:
+            self._mongo_collection_prefix = parsed_path[1]
+        else:
+            self._mongo_collection_prefix = None
 
     @property
     def _connection(self):
@@ -24,31 +36,67 @@ class MongodbStorage(BaseStorage):
     def _database(self):
         return self._connection[self._mongo_db_name]
 
+    @property
+    def _collection(self):
+        if self._mongo_collection_prefix is not None:
+            return self._database[self._mongo_collection_prefix]
+        return self._database
+
+    def _list_collections(self, strip_prefix=True):
+        """
+        List all collections with name starting with the
+        configured prefix.
+        """
+
+        if self._mongo_collection_prefix:
+            prefix = self._mongo_collection_prefix + '.'
+        else:
+            prefix = None
+
+        for name in self._database.collection_names():
+            if name.startswith('system.'):
+                # Ignore system collections
+                continue
+
+            if prefix is None:
+                yield name
+
+            else:
+                if name.startswith(prefix):
+                    if strip_prefix:
+                        yield name[len(prefix):]
+                    else:
+                        yield name
+
     def _flush_db(self):
-        self._connection.drop_database(self._mongo_db_name)
+        if self._mongo_collection_prefix is None:
+            self._connection.drop_database(self._mongo_db_name)
+        else:
+            # Just drop tables matching prefix
+            for name in self._list_collections(strip_prefix=False):
+                self._database.drop_collection(name)
 
     def list_object_types(self):
-        return [c for c in self._database.collection_names()
-                if not c.startswith('system.')]
+        return list(self._list_collections())
 
     def list_objects(self, obj_type):
         if obj_type not in self.list_object_types():
             raise ValueError('Missing object')
-        coll = self._database[obj_type]
+        coll = self._collection[obj_type]
         return [o['_id'] for o in coll.find(fields=['_id'])]
 
     def get_object(self, obj_type, obj_id):
-        coll = self._database[obj_type]
+        coll = self._collection[obj_type]
         obj = coll.find_one(obj_id)
         obj.pop('_id', None)
         return obj
 
     def set_object(self, obj_type, obj_id, obj):
-        coll = self._database[obj_type]
+        coll = self._collection[obj_type]
         obj['_id'] = obj_id
         coll.update({'_id': obj['_id']}, obj, upsert=True)
         return self.get_object(obj_type, obj_id)
 
     def del_object(self, obj_type, obj_id):
-        coll = self._database[obj_type]
+        coll = self._collection[obj_type]
         coll.remove({'_id': obj_id})
