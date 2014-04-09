c7f03a047fab282ad33fc47d97a79c46a0eda372
Samuele Santi <redshadow@hackzine.org>
2014-04-09 15:47:46 +0200

    Crawler plugin for odt/statistica

--- harvester/ext/crawlers/odt/pat_statistica.py
+++ harvester/ext/crawlers/odt/pat_statistica.py
@@ -1,6 +1,38 @@
-class Statistica(object):
-    pass
+import logging
+
+from harvester.ext.crawlers.base import HarvesterPluginBase
+
+from .pat_statistica_client import (StatisticaClient, StatisticaSubproClient,
+                                    CATEGORIES, ORGANIZATIONS,
+                                    dataset_statistica_to_ckan,
+                                    dataset_statistica_subpro_to_ckan)
 
 
-class StatisticaSubPro(object):
-    pass
+class Statistica(HarvesterPluginBase):
+    logger = logging.getLogger(__name__)
+
+    def fetch_data(self, storage):
+        self.logger.info("Fetching data from statistica")
+
+        client = StatisticaClient()
+        for dataset in client.iter_datasets(clean=False):
+            self.logger.info('Got dataset: {0}'.format(dataset['id']))
+            storage.set_object('dataset', dataset['id'], dataset)
+
+    def to_ckan(self, storage_in, storage_out):
+        pass
+
+
+class StatisticaSubPro(HarvesterPluginBase):
+    logger = logging.getLogger(__name__)
+
+    def fetch_data(self, storage):
+        self.logger.info("Fetching data from statistica-subpro")
+
+        client = StatisticaSubproClient()
+        for dataset in client.iter_datasets(clean=False):
+            self.logger.info('Dataset: {0}'.format(dataset['id']))
+            storage.set_object('dataset', dataset['id'], dataset)
+
+    def to_ckan(self, storage_in, storage_out):
+        pass
