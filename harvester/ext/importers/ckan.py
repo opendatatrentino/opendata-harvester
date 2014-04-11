d02004a7a544ba90e96ad814f58cf7ce948d2956
Samuele Santi <redshadow@hackzine.org>
2014-04-11 16:36:40 +0200

    Added actual code to import stuff

--- harvester/ext/importers/ckan.py
+++ harvester/ext/importers/ckan.py
@@ -17,6 +17,20 @@ class CkanImporter(ImporterPluginBase):
 
         # todo: load all data from the storage, or make it accessible as a dict
         # todo: we need to preserve names on update
+        # todo: improve the sync client to make it more extensible
 
         data = {'dataset': {}, 'group': {}, 'organization': {}}
+
+        for obj_id in storage.list_objects('dataset'):
+            obj = storage.get_object('dataset', obj_id)
+            data['dataset'][obj_id] = obj
+
+        for obj_id in storage.list_objects('group'):
+            obj = storage.get_object('group', obj_id)
+            data['group'][obj_id] = obj
+
+        for obj_id in storage.list_objects('organization'):
+            obj = storage.get_object('organization', obj_id)
+            data['organization'][obj_id] = obj
+
         client.sync(source_name, data)
