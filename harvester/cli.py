6f7a17b34829a1aaf9cac39ec7e03ddb2b616b5d
Samuele Santi <redshadow@hackzine.org>
2014-04-17 18:07:19 +0200

    Nicer colors

--- harvester/cli.py
+++ harvester/cli.py
@@ -4,6 +4,8 @@ import sys
 from cliff.app import App
 from cliff.commandmanager import CommandManager
 
+from .utils import ColorLogFormatter
+
 
 class HarvesterApp(App):
     logger = logging.getLogger(__name__)
@@ -13,6 +15,36 @@ class HarvesterApp(App):
             description='Harvester application CLI',
             version='0.1',
             command_manager=CommandManager('harvester.commands'))
+
+    def configure_logging(self):
+        """
+        Create logging handlers for any log output.
+        Modified version to set custom formatter for console
+        """
+        root_logger = logging.getLogger('')
+        root_logger.setLevel(logging.DEBUG)
+
+        # Set up logging to a file
+        if self.options.log_file:
+            file_handler = logging.FileHandler(
+                filename=self.options.log_file,
+            )
+            formatter = logging.Formatter(self.LOG_FILE_MESSAGE_FORMAT)
+            file_handler.setFormatter(formatter)
+            root_logger.addHandler(file_handler)
+
+        # Always send higher-level messages to the console via stderr
+        console = logging.StreamHandler(self.stderr)
+        console_level = {0: logging.WARNING,
+                         1: logging.INFO,
+                         2: logging.DEBUG,
+                         }.get(self.options.verbose_level, logging.DEBUG)
+        console.setLevel(console_level)
+        # formatter = logging.Formatter(self.CONSOLE_MESSAGE_FORMAT)
+        formatter = ColorLogFormatter()  # This one is nicer!
+        console.setFormatter(formatter)
+        root_logger.addHandler(console)
+        return
 
 
 def main(argv=sys.argv[1:]):
--- harvester/utils.py
+++ harvester/utils.py
@@ -2,9 +2,13 @@
 Miscellaneous utilities
 """
 
-import urlparse
+import logging
+import sys
 
 from stevedore.extension import ExtensionManager
+
+
+POWERLINE_STYLE = True
 
 
 def get_plugin(plugin_type, url, options):
@@ -90,3 +94,66 @@ def parse_plugin_url(url):
 
     # We don't have neither of ``+`` or ``://``
     return url, None
+
+
+try:
+    from termcolor import colored
+except ImportError:
+    def colored(text, *a, **kw):
+        return text
+
+
+def _colorer(*args, **kwargs):
+    return lambda x: colored(x, *args, **kwargs)
+
+
+class ColorLogFormatter(logging.Formatter):
+    level_colors = {
+        logging.DEBUG: ('white', 'blue', 'cyan'),
+        logging.INFO: ('white', 'green', 'green'),
+        logging.WARNING: ('white', 'yellow', 'yellow'),
+        logging.ERROR: ('white', 'red', 'red'),
+        logging.CRITICAL: ('yellow', 'red', 'red'),
+    }
+
+    def _get_exc_info(self, record):
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            try:
+                return unicode(record.exc_text)
+            except UnicodeError:
+                # Sometimes filenames have non-ASCII chars, which can lead
+                # to errors when s is Unicode and record.exc_text is str
+                # See issue 8924.
+                # We also use replace for when there are multiple
+                # encodings, e.g. UTF-8 for the filesystem and latin-1
+                # for a script. See issue 13232.
+                return record.exc_text.decode(sys.getfilesystemencoding(),
+                                              'replace')
+        return None
+
+    def format(self, record):
+        """Format logs nicely"""
+
+        fgcolor, bgcolor, msgcolor = \
+            self.level_colors.get(record.levelno, 'white')
+        levelname = colored(' {0:<6} '.format(record.levelname),
+                            fgcolor, 'on_' + bgcolor)
+        if POWERLINE_STYLE:
+            levelname += colored(u'\ue0b0', bgcolor)
+
+        message = colored(record.getMessage(), msgcolor)
+
+        s = ' '.join((levelname, message))
+
+        exc_info = self._get_exc_info(record)
+        if exc_info is not None:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s += colored(exc_info, 'grey', attrs=['bold'])
+
+        return s
