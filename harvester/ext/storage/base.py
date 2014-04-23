a3e5abba59e30db9c7228a8a2e71b8ad5137d487
Samuele Santi <redshadow@hackzine.org>
2014-04-23 18:25:44 +0200

    WIP storage refactoring

--- harvester/ext/storage/base.py
+++ harvester/ext/storage/base.py
@@ -1,5 +1,4 @@
-import abc
-# import collections
+import collections
 
 
 class StorageError(Exception):
@@ -11,82 +10,114 @@ class NotFound(StorageError):
 
 
 class BaseStorage(object):
-    __metaclass__ = abc.ABCMeta
+    """
+    Storages expose a common API to store data in
+    different backends.
+
+    A storage should be able to manage three kinds of
+    objects:
+
+    - **document**: a JSON object
+    - **blob**: a large object, to be stored as-is
+    - **keyval**: a key/value pair, usually for storing
+      information about the storage data itself.
+
+    The (new) API for accessing objects is:
+
+    storage.documents <-- dict-like
+    storage.blobs <-- dict-like
+    storage.keyval <-- dict-like
+    storage.info <-- dict-like (alias for ``.keyval['info']``)
+    """
+
+    document_bucket_class = None
+    blob_bucket_class = None
+    keyval_bucket_class = None
 
     def __init__(self, url, conf=None):
         self.url = url
         self.conf = conf or {}
 
-    @abc.abstractmethod
-    def list_object_types(self):
-        pass
+    @property
+    def documents(self):
+        return BaseBucketManager(self, self.document_bucket_class)
 
-    @abc.abstractmethod
-    def list_objects(self, obj_type):
-        pass
+    @property
+    def blobs(self):
+        return BaseBucketManager(self, self.blob_bucket_class)
 
-    @abc.abstractmethod
-    def get_object(self, obj_type, obj_id):
-        pass
+    @property
+    def keyvals(self):
+        return BaseBucketManager(self, self.keyval_bucket_class)
 
-    @abc.abstractmethod
-    def set_object(self, obj_type, obj_id, obj):
-        pass
+    @property
+    def info(self):
+        return self.keyval['info']
 
-    @abc.abstractmethod
-    def del_object(self, obj_type, obj_id):
-        pass
-
-    def iter_objects(self, obj_type):
-        for objid in self.list_objects(obj_type):
-            yield self.get_object(obj_type, objid)
-
-    # todo: we need to store metadata too
+    def flush_storage(self):
+        raise NotImplementedError(
+            "This storage does not support flushing")
 
 
-# We need the objects below in order to use the sync client
+class BaseBucketManager(collections.MutableMapping):
+    """
+    A bucket manager simulate a dictionary of buckets
+    for a given storage.
+    """
+
+    def __init__(self, storage, bucket_class):
+        self.storage = storage
+        self.bucket_class = bucket_class
+
+    def __getitem__(self, bucket_name):
+        return self.bucket_class(self.storage, bucket_name)
+
+    def __setitem__(self, name, value):
+        raise NotImplementedError("Setting a bucket is not supported")
+
+    def __delitem__(self, name):
+        raise NotImplementedError("Deleting a bucket is not supported")
+
+    def __iter__(self):
+        return self.bucket_class.list_buckets(self.storage)
+
+    def __len__(self):
+        return len(list(self.__iter__()))
 
 
-# class StorageDatabase(collections.MutableMapping):
-#     def __init__(self, storage):
-#         self.storage = storage
+class BaseBucket(collections.MutableMapping):
+    """
+    Base for bucket objects, which are dict-likes mapping
+    keys to objects.
 
-#     def __getitem__(self, name):
-#         return StorageCollection(self, name)
+    Methods left abstract / needing implementation:
 
-#     def __setitem__(self, name, value):
-#         raise NotImplementedError("Cannot set collection")
+    - classmethod: list_buckets(cls, storage)
+    - __getitem__, __setitem__, __delitem__, __iter__, __len__
+    """
 
-#     def __delitem__(self, name):
-#         raise NotImplementedError("Cannot delete collection")
+    def __init__(self, storage, myname):
+        self.storage = storage
+        self.myname = myname
 
-#     def __iter__(self):
-#         return iter(self.storage.list_object_types())
+    @classmethod
+    def list_buckets(cls, storage):
+        """List buckets of this type in a given storage"""
+        raise NotImplementedError
 
-#     def __len__(self):
-#         return len(self.storage.list_object_types())
+
+class BaseDocumentBucket(BaseBucket):
+    pass
 
 
-# class StorageCollection(collections.MutableMapping):
-#     def __init__(self, parent, name):
-#         self.parent = parent
-#         self.name = name
+class BaseBlobBucket(BaseBucket):
+    pass
 
-#     @property
-#     def storage(self):
-#         return self.parent.storage
 
-#     def __getitem__(self, name):
-#         return self.storage.get_object(self.name, name)
+class BaseKeyvalBucket(BaseBucket):
+    pass
 
-#     def __setitem__(self, name, value):
-#         return self.storage.set_object(self.name, name, value)
 
-#     def __delitem__(self, name):
-#         return self.storage.del_object(self.name, name)
-
-#     def __iter__(self):
-#         return iter(self.storage.list_objects(self.name))
-
-#     def __len__(self):
-#         return len(self.storage.list_objects(self.name))
+BaseStorage.document_bucket_class = BaseDocumentBucket
+BaseStorage.blob_bucket_class = BaseBlobBucket
+BaseStorage.keyval_bucket_class = BaseKeyvalBucket
--- harvester/ext/storage/mongodb.py
+++ harvester/ext/storage/mongodb.py
@@ -2,7 +2,8 @@ import urlparse
 
 import pymongo
 
-from .base import BaseStorage
+from .base import (BaseStorage, BaseDocumentBucket, BaseBlobBucket,
+                   BaseKeyvalBucket)
 
 
 class MongodbStorage(BaseStorage):
@@ -38,22 +39,28 @@ class MongodbStorage(BaseStorage):
     def _database(self):
         return self._connection[self._mongo_db_name]
 
-    @property
-    def _collection(self):
-        if self._mongo_collection_prefix is not None:
-            return self._database[self._mongo_collection_prefix]
-        return self._database
+    def _get_collection(self, name):
+        """Return a sub-collection object, by name"""
+        prefix = self._mongo_collection_prefix
+        root = self._database[prefix] if prefix else self._database
+        return root[name]
 
-    def _list_collections(self, strip_prefix=True):
+    def _get_collection_name(self, name):
+        if self._mongo_collection_prefix:
+            return '.'.join((self._mongo_collection_prefix, name))
+        return name
+
+    def _list_sub_collections(self, prefix, strip_prefix=True):
         """
         List all collections with name starting with the
         configured prefix.
+
+        :param strip_prefix:
+            if set to True (the default) will strip the prefix from returned
+            collection names
         """
 
-        if self._mongo_collection_prefix:
-            prefix = self._mongo_collection_prefix + '.'
-        else:
-            prefix = None
+        prefix = (prefix + '.') if prefix else None
 
         for name in self._database.collection_names():
             if name.startswith('system.'):
@@ -63,27 +70,104 @@ class MongodbStorage(BaseStorage):
             if prefix is None:
                 yield name
 
-            else:
-                if name.startswith(prefix):
-                    if strip_prefix:
-                        yield name[len(prefix):]
-                    else:
-                        yield name
+            elif name.startswith(prefix):
+                if strip_prefix:
+                    name = name[len(prefix):]
+                yield name
 
-    def _flush_db(self):
-        if self._mongo_collection_prefix is None:
-            self._connection.drop_database(self._mongo_db_name)
-        else:
-            # Just drop tables matching prefix
-            for name in self._list_collections(strip_prefix=False):
-                self._database.drop_collection(name)
+    def _list_our_collections(self):
+        """List full names of our collection"""
 
-    def list_object_types(self):
-        return list(self._list_collections())
+        prefix = self._mongo_collection_prefix
+        prefix = (prefix + '.') if prefix else None
 
-    def list_objects(self, obj_type):
-        if obj_type not in self.list_object_types():
+        for name in self._database.collection_names():
+            if name.startswith('system.'):
+                # Ignore system collections
+                continue
+
+            if (prefix is None) or name.startswith(prefix):
+                yield name
+
+    def _list_buckets(self, bucket_type):
+        """List all the sub-collections matching a given prefix"""
+
+        full_prefix = bucket_type + '.'
+        if self._mongo_collection_prefix:
+            full_prefix = '.'.join((
+                self._mongo_collection_prefix, full_prefix))
+
+        for name in self._database.collection_names():
+            if name.startswith('system.'):
+                # Skip system collections..
+                continue
+
+            if name.startswith(full_prefix):
+                yield name[len(full_prefix):]
+
+    # def _list_collections(self, strip_prefix=True):
+    #     return self._list_sub_collections(
+    #         self._mongo_collection_prefix, strip_prefix=strip_prefix)
+
+    # def flush_storage(self):
+    #     if self._mongo_collection_prefix is None:
+    #         # We can directly drop database
+    #         self._connection.drop_database(self._mongo_db_name)
+
+    #     else:
+    #         # Just drop tables matching prefix..
+    #         for name in self._list_collections(strip_prefix=False):
+    #             self._database.drop_collection(name)
+
+    # def list_object_types(self):
+    #     return list(self._list_collections())
+
+    # def list_objects(self, obj_type):
+    #     if obj_type not in self.list_object_types():
+    #         raise ValueError('Missing object')
+    #     coll = self._collection[obj_type]
+    #     return [o['_id'] for o in coll.find(fields=['_id'])]
+
+    # def get_object(self, obj_type, obj_id):
+    #     coll = self._collection[obj_type]
+    #     obj = coll.find_one(obj_id)
+    #     obj.pop('_id', None)
+    #     return obj
+
+    # def set_object(self, obj_type, obj_id, obj):
+    #     coll = self._collection[obj_type]
+    #     obj['_id'] = obj_id
+    #     coll.update({'_id': obj['_id']}, obj, upsert=True)
+    #     return self.get_object(obj_type, obj_id)
+
+    # def del_object(self, obj_type, obj_id):
+    #     coll = self._collection[obj_type]
+    #     coll.remove({'_id': obj_id})
+
+
+class MongoDocumentBucket(BaseDocumentBucket):
+    @classmethod
+    def list_buckets(cls, storage):
+        """
+        To list buckets we want to list all the sub-collections
+        matching the 'document.' prefix.
+        """
+        return storage._list_buckets('document')
+
+    @property
+    def _collection_name(self):
+        """Get the name of the collection name for this kind of documents"""
+        return self.storage._get_collection_name(
+            '.'.join(('document', self.myname)))
+
+    @property
+    def _collection(self):
+        return self.storage.get_dataset
+
+    def __iter__(self):
+        if self.mytype not in self.list_buckets(self.storage):
             raise ValueError('Missing object')
+
         coll = self._collection[obj_type]
         return [o['_id'] for o in coll.find(fields=['_id'])]
 
@@ -102,3 +186,6 @@ class MongodbStorage(BaseStorage):
     def del_object(self, obj_type, obj_id):
         coll = self._collection[obj_type]
         coll.remove({'_id': obj_id})
+
+
+MongodbStorage.document_bucket_class = MongoDocumentBucket
