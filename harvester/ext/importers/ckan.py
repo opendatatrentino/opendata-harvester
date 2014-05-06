021b28bf6c5c2db2a8abddd431f6c8f1c1d158eb
Samuele Santi <redshadow@hackzine.org>
2014-05-06 14:56:11 +0200

    Ckan importer now exposing configuration options from synchronization client (fix #2)

--- harvester/ext/importers/ckan.py
+++ harvester/ext/importers/ckan.py
@@ -9,8 +9,17 @@ class CkanImporter(ImporterPluginBase):
     logger = logging.getLogger(__name__)
 
     options = [
-        ('api_key', 'str', None),
-        ('source_name', 'str', None),
+        ('api_key', 'str', None, "Ckan API key"),
+        ('source_name', 'str', None, "Name for the import source"),
+        ('organization_merge_strategy', 'str', None, "One of: create, update"),
+        ('group_merge_strategy', 'str', None, "One of: create, update"),
+        ('dataset_preserve_names', 'bool', True,
+         "if ``True`` (the default) will preserve old names of "
+         "existing datasets"),
+        ('dataset_preserve_organization', 'bool', True,
+         "Preserve current dataset organization?"),
+        ('dataset_group_merge_strategy', 'str', 'add',
+         "One of: add, replace, preserve")
     ]
 
     def sync_data(self, storage):
@@ -18,24 +27,29 @@ class CkanImporter(ImporterPluginBase):
 
         api_key = self.conf['api_key']
         source_name = self.conf['source_name']
-        client = SynchronizationClient(self.url, api_key)
 
-        # todo: load all data from the storage, or make it accessible as a dict
-        # todo: we need to preserve names on update
-        # todo: improve the sync client to make it more extensible
+        # ------------------------------------------------------------
+        # Initialize the client taking some kwargs from configuration
 
-        data = {'dataset': {}, 'group': {}, 'organization': {}}
+        copy_conf = [
+            'organization_merge_strategy',
+            'group_merge_strategy',
+            'dataset_preserve_names',
+            'dataset_preserve_organization',
+            'dataset_group_merge_strategy',
+        ]
+        kwargs = {}
+        for k in copy_conf:
+            kwargs[k] = self.conf[k]
+        client = SynchronizationClient(self.url, api_key, **kwargs)
 
-        for obj_id in storage.documents['dataset']:
-            obj = storage.documents['dataset'][obj_id]
-            data['dataset'][obj_id] = obj
+        # ------------------------------------------------------------
+        # New-style storage is dict-like, no need to pre-query.
 
-        for obj_id in storage.documents['group']:
-            obj = storage.documents['group'][obj_id]
-            data['group'][obj_id] = obj
-
-        for obj_id in storage.documents['organization']:
-            obj = storage.documents['organization'][obj_id]
-            data['organization'][obj_id] = obj
+        data = {
+            'dataset': storage.documents['dataset'],
+            'group': storage.documents['group'],
+            'organization': storage.documents['organization'],
+        }
 
         client.sync(source_name, data)
