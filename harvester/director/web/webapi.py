f4e0cb856dcf40ab77b23f7670ab7378e17487fb
Samuele Santi <redshadow@hackzine.org>
2014-05-28 16:21:21 +0200

    [#7] More work on the harvester director API

--- harvester/director/web/webapi.py
+++ harvester/director/web/webapi.py
@@ -44,10 +44,12 @@ def _get_request_object():
     raise BadRequest('Unsupported request content type: {0}'.format(ctype))
 
 
-class SourceList(restful.Resource):
+class PluginConfResourceList(restful.Resource):
+    bucket_name = None
+
     def get(self):
         storage = _get_storage('conf')
-        all_objects = storage.documents['source'].iteritems()
+        all_objects = storage.documents[self.bucket_name].iteritems()
         return [
             {'id': key,
              'label': obj.get('label'),
@@ -56,10 +58,8 @@ class SourceList(restful.Resource):
             for key, obj in all_objects]
 
 
-class Source(restful.Resource):
+class PluginConfResource(restful.Resource):
     """
-    A source is used to hold configuration for the crawler plugin.
-
     Fields:
 
     - id -> generated id
@@ -68,10 +68,12 @@ class Source(restful.Resource):
     - options -> dictionary of options to be passed to the crawler
     """
 
+    bucket_name = None
+
     def get(self, objid):
         storage = _get_storage('conf')
         try:
-            obj = storage.documents['source'][objid]
+            obj = storage.documents[self.bucket_name][objid]
         except StorageObjNotFound:
             raise NotFound('Object not found')
         return {'id': objid,
@@ -92,9 +94,9 @@ class Source(restful.Resource):
         if not isinstance(new_obj['options'], dict):
             raise BadRequest('Options must be a dict')
 
-        storage.documents['source'][objid] = new_obj
+        storage.documents[self.bucket_name][objid] = new_obj
 
-        obj = storage.documents['source'][objid]
+        obj = storage.documents[self.bucket_name][objid]
         obj['id'] = objid
 
         return obj, 201  # todo: return 201 for creation only
@@ -104,35 +106,100 @@ class Source(restful.Resource):
 
     def delete(self, objid):
         storage = _get_storage('conf')
-        del storage.documents['source'][objid]
+        del storage.documents[self.bucket_name][objid]
 
 
-class DestinationList(restful.Resource):
+class CrawlerConfList(PluginConfResourceList):
+    bucket_name = 'source'
+
+
+class CrawlerConf(PluginConfResource):
+    bucket_name = 'source'
+
+
+class ImporterConfList(PluginConfResourceList):
+    bucket_name = 'destination'
+
+
+class ImporterConf(PluginConfResource):
+    bucket_name = 'destination'
+
+
+class ConverterConfList(PluginConfResourceList):
+    bucket_name = 'converter'
+
+
+class ConverterConf(PluginConfResource):
+    bucket_name = 'converter'
+
+
+class TaskListResource(restful.Resource):
+    """
+    Resource to list jobs (on GET) and trigger execution (on POST)
+    """
+
     def get(self):
+        # Return list of jobs.
+        # todo: we need pagination support here..
         return {'hello': None}
 
+    def post(self):
+        rqobj = _get_request_object()
 
-class Destination(restful.Resource):
-    def get(self, objid):
-        return {'hello': objid}
+        if rqobj['type'] == 'crawl':
+            return self._run_task_crawl(rqobj)
+
+        if rqobj['type'] == 'convert':
+            return self._run_task_convert(rqobj)
+
+        if rqobj['type'] == 'import':
+            return self._run_task_import(rqobj)
+
+        if rqobj['type'] == 'preview':
+            return self._run_task_preview(rqobj)
+
+    def _run_task_crawl(self, rqobj):
+        # todo: create job record
+        # todo: trigger execution via celery worker
+        # todo: return information about the started job
+        pass
+
+    def _run_task_convert(self, rqobj):
+        # todo: create job record
+        # todo: trigger execution via celery worker
+        # todo: return information about the started job
+        pass
+
+    def _run_task_import(self, rqobj):
+        # todo: create job record
+        # todo: trigger execution via celery worker
+        # todo: return information about the started job
+        pass
+
+    def _run_task_preview(self, rqobj):
+        # todo: create job record
+        # todo: trigger execution via celery worker
+        # todo: return information about the started job
+        pass
 
 
-class JobList(restful.Resource):
-    def get(self):
-        return {'hello': None}
+class TaskResource(restful.Resource):
+    """
+    Resource used to get information about jobs.
+    """
 
-
-class Job(restful.Resource):
     def get(self, objid):
         # Four kind of jobs: crawl, convert, preview, import
         return {'hello': objid}
 
 
-api.add_resource(SourceList, '/sources/')
-api.add_resource(Source, '/sources/<objid>/')
-api.add_resource(DestinationList, '/destinations/')
-api.add_resource(Destination, '/destinations/<objid>/')
-api.add_resource(JobList, '/jobs/')
-api.add_resource(Job, '/jobs/<objid>/')
+api.add_resource(CrawlerConfList, '/conf/crawler/')
+api.add_resource(CrawlerConf, '/conf/crawler/<objid>/')
+api.add_resource(ConverterConfList, '/conf/converter/')
+api.add_resource(ConverterConf, '/conf/converter/<objid>/')
+api.add_resource(ImporterConfList, '/conf/importer/')
+api.add_resource(ImporterConf, '/conf/importer/<objid>/')
+api.add_resource(TaskListResource, '/task/')
+api.add_resource(TaskResource, '/task/<objid>/')
 
 # todo: we want to return a 404 w/ json payload on all missing endpoints
