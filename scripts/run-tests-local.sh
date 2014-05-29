b9221adeed4882807d202ca26554de10e96f3c09
Samuele Santi <redshadow@hackzine.org>
2014-05-29 11:55:58 +0200

    [#7] More facilities for running director tests

---
+++ scripts/run-tests-local.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MONGO_HOST=database.local
+
+TEST_DB_PREFIX="$( date '+%s' )"
+TEST_DB_HARVESTER="test_${TEST_DB_PREFIX}_harvester"
+TEST_DB_DIRECTOR="test_${TEST_DB_PREFIX}_director"
+
+export MONGO_URL=mongodb://"$MONGO_HOST":27017/"${TEST_DB_HARVESTER}"/testdata
+export HARVESTER_SETTINGS="$( tempfile -s .py )"
+
+# Generate test settings
+# ----------------------------------------
+
+cat > "$HARVESTER_SETTINGS" <<EOF
+from harvester.director.web.settings.development import *
+
+DEBUG = True
+HARVESTER_MONGODB = {
+    'host': 'mongodb://${MONGO_HOST}:27017',
+    'name': '${TEST_DB_DIRECTOR}',
+}
+CELERY_BROKER_URL = 'redis://localhost:6379'
+EOF
+
+# Run tests
+# ----------------------------------------
+tox
+
+# Cleanup
+# ----------------------------------------
+rm "$HARVESTER_SETTINGS"
+echo -e 'use ${TEST_DB_HARVESTER}\ndb.dropDatabase()' | mongo "$MONGO_HOST"
+echo -e 'use ${TEST_DB_DIRECTOR}\ndb.dropDatabase()' | mongo "$MONGO_HOST"
---
+++ scripts/travis/testing_settings.py
@@ -0,0 +1,9 @@
+from harvester.director.web.settings.development import *  # noqa
+
+
+DEBUG = True
+HARVESTER_MONGODB = {
+    'host': 'mongodb://localhost:27017',
+    'name': 'test_harvester_director',
+}
+CELERY_BROKER_URL = 'redis://localhost:6379'
